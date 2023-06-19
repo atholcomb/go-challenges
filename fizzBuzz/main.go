8994913a180c52c317bb3617c78a785a27c01487
Andrew Holcomb <atholcomb@outlook.com>
2023-06-19 05:59:09 -0600

    fizzBuzz refactored

--- fizzBuzz/main.go
+++ fizzBuzz/main.go
@@ -2,32 +2,56 @@
 // main.go
 // Write a function (fizzBuzz), that
 // returns FizzBuzz, Fizz, or Buzz
-// if a number is divisible by 3, 5 or 15
+// for all factors of 3, 5 or both 3 and 5
 
 package main
 
 import (
   "fmt"
+  "strconv"
 )
 
 func main() {
-  fmt.Println(fizzBuzz())
+  fizzBuzz()
 }
 
-func fizzBuzz() (int, string) {
-  var num int
-  n, m, o := 3, 5, 15
+func fizzBuzz() {
+  var fbcount, fzcount, bzcount int
+  var fbList, fzList, bzList []string
+  var numList [31]int
 
-  fmt.Printf("Please enter a number: ")
-  fmt.Scanf("%v", &num)
-
-  if num % o == 0 {
-    return num, "Fizzbuzz"
-  } else if num % n == 0 {
-    return num, "Fizz"
-  } else if num % m == 0 {
-    return num, "Buzz"
-  } else {
-    return num, "is not divisble by 3, 5, or 15"
+  /* test for Fizzbuzz, Fizz and Buzz in range numList */
+  for num := range numList { 
+    if num % 3 == 0 && num % 5 == 0 {
+      fbList = append(fbList, "Fizzbuzz: " + strconv.Itoa(num))
+    } else if num % 3 == 0 {
+      fzList = append(fzList, "Fizz: " + strconv.Itoa(num))
+    } else if num % 5 == 0 {
+      bzList = append(bzList, "Buzz: " + strconv.Itoa(num))
+    }
   }
+
+  /* output all factors divisble by 3 */
+  fmt.Print("Divisble by 3:\n")
+  for _, v := range fzList {
+    fzcount++
+    fmt.Printf("%v\n", v)
+  }
+  fmt.Printf("%s%d\n","Total count: ", fzcount)
+
+  /* output all factors divisble by 5 */
+  fmt.Print("\nDivisble by 5:\n")
+  for _, v := range bzList {
+    bzcount++
+    fmt.Printf("%v\n", v)
+  }
+  fmt.Printf("%s%d\n","Total count: ", bzcount)
+
+  /* output all factors divisble by both 3 and 5 */
+  fmt.Print("\nDivisble by both 3 and 5:\n")
+  for _, v := range fbList {
+    fbcount++
+    fmt.Printf("%v\n", v)
+  }
+  fmt.Printf("%s%d\n\n","Total count: ", fbcount)
 }
